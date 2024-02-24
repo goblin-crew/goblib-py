2924254aa154aa33858d6f9c0b24473b2c50938a
slowlorizz <slowloris.tech@gmail.com>
2024-02-24 21:29:02 +0100

    update

--- goblib/sec/net/wifi/proberequests/models/Scanner.py
+++ goblib/sec/net/wifi/proberequests/models/Scanner.py
@@ -1,6 +1,6 @@
 from scapy.packet import Packet
 from scapy.layers import dot11 as d11
-from scapy.sendrecv import AsyncSniffer
+from scapy.sendrecv import sniff
 
 from goblib.sec.net.wifi.proberequests.models import ProbeRequest
 from goblib.sec.net.wifi.proberequests.utils import WifiInterface
@@ -11,7 +11,7 @@ class Scanner:
     '''
     A Wifi-ProbeRequest-Scanner \n
     uses scapy.sendrecv.AsyncSniffer with preconfigured values and results return as ProbeRequest-Objects \n
-    (!!!) is async
+
 
     @property data          <list[ProbeRequest]>            collected ProbeRequest-Packets during scan
     @property __sniffer__   <scapy.sendrecv.AsyncSniffer>   internal obj for sniffing
@@ -27,8 +27,14 @@ class Scanner:
         self.data: list[ProbeRequest] = []
         self.quiet: bool = quiet
         self.wifi_iface = WifiInterface(iface)
-        self.__started__: bool = False
-        self.__sniffer__: AsyncSniffer = AsyncSniffer(iface=iface, count=count, store=True, prn=self.__handle_packet__, monitor=True, quiet=ignore_sniffer_errors, started_callback=self.__set_started__)
+        self.stop: bool = False
+
+        self.__config__: dict = {
+            'iface': iface,
+            'count': count,
+            'quiet': ignore_sniffer_errors
+        }
+        
 
     def __handle_packet__(self, pkt: Packet):
         if isinstance(pkt, Packet):
@@ -38,34 +44,10 @@ class Scanner:
 
                 if not self.quiet:
                     print(f"({prq.ts_str})\t[{prq.mac}]\t{prq.ssid}")
-
-    def __set_started__(self) -> None:
-        self.__started__ = True
-
     
-    def start(self) -> Self:
+    def scan(self) -> Self:
         if self.wifi_iface.in_managed_mode:
             self.wifi_iface.set_mode_monitor()
 
-        self.__sniffer__.start()
+        sniff(iface=self.__config__['iface'], count=self.__config__['count'], store=True, prn=self.__handle_packet__, monitor=True, quiet=self.__config__['quiet'])
         return self
-    
-
-    def stop(self, join: bool = True) -> Self:
-        self.__sniffer__.stop(join=join)
-        self.wifi_iface.set_mode_managed()
-        return self
-    
-
-    def await_join(self) -> Self:
-        self.__sniffer__.join()
-        self.wifi_iface.set_mode_managed()
-        return self
-    
-    @property
-    def running(self) -> bool:
-        return self.__sniffer__.running
-    
-    @property
-    def started(self) -> bool:
-        return self.__started__
--- proberequest_scanner.py
+++ proberequest_scanner.py
@@ -6,31 +6,4 @@ config = dotenv_values('.env')
 
 scanner = PRQ.Scanner(config['IFACE'])
 
-print("start scanner....")
-scanner.start()
-
-t = 0
-while not scanner.started:
-    print(f"\rwaiting for scanner start... ({t}s)", end='')
-    time.sleep(0.5)
-    t += 0.5
-
-print()
-
-for i in range(0, 120):
-    print(f"\rscanning ({i}s)", end='')
-    time.sleep(1)
-
-print()
-
-print("time elapsed")
-print("stopping scanner....")
-scanner.stop(True)
-print("scanner stopped!")
-
-print("scanner results:")
-
-for i in scanner.data:
-    print(f"{i.timestamp}\t[{i.mac}]\t{i.ssid}")
-
-print("\n")
+scanner.scan()
